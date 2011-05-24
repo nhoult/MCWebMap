1e5e8c9b079a4049a1d2604ca995ae9aa7f15824
Nathan Hoult <nhoult51@gmail.com>
2011-05-23 22:51:46 -0500

    removed some old files

--- src/main/java/mcwebmap/MapImageGeneratorCB.java
+++
@@ -1,8 +0,0 @@
-package mcwebmap;
-
-public interface MapImageGeneratorCB {
-	// for things like console out and such
-	public void message(String message);
-	// for a progress bar, 0-100
-	public void percentDone(int percent);
-}
